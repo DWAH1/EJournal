5fbf2e26e0d6ea60f7849c9df4ab9164e4fffc13
Ser <seregax517@gmail.com>
2017-05-11 22:35:07 +0300

    bower

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,3 +1,11 @@
 /**
  * Created by ser on 11.05.17.
  */
+app.controller('ctrl-init', function($scope, $http) {
+
+    $http.get("https://teacher-journal2.herokuapp.com/students.json").then(function (response) {
+        console.log(response);
+        $scope.students = response.data;
+    });
+
+});
