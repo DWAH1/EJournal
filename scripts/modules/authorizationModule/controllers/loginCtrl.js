e4c2cc4b88c32d824d8d1d061c54f56538dd7efc
Ser <seregax517@gmail.com>
2017-05-28 23:50:41 +0300

    design login

--- scripts/modules/authorizationModule/controllers/loginCtrl.js
+++ scripts/modules/authorizationModule/controllers/loginCtrl.js
@@ -1,11 +1,35 @@
 authorizationModule.controller('loginCtrl', ['$scope', 'authorizationFactory', '$location',
     function($scope, authorizationFactory, $location) {
+
+        let loginJBox = new jBox('Modal', {
+            width: 400,
+            animation: 'pulse',
+            content: $('#loginPopup')
+        });
+
         $scope.loginClick = function() {
 
             if (authorizationFactory.login($scope.login, $scope.pass)) {
+                loginJBox.close();
                 $location.path('/reports');
             } else {
                 alert('Access denied!');
             }
-        }
+        };
+
+        $scope.openLoginPopup = function () {
+            loginJBox.open();
+        };
+
+        $scope.openAboutPopup = function () {
+
+            let aboutJBox = new jBox('Modal', {
+                width: 400,
+                animation: 'pulse',
+                content: $('#aboutPopup')
+            });
+
+            aboutJBox.open();
+        };
+
 }]);
