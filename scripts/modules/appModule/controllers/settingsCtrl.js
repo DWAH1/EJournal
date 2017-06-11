8d1d0e3629c8365ece7011057dfd0c9e0077fec4
Ser <seregax517@gmail.com>
2017-06-11 14:40:57 +0300

    san

--- scripts/modules/appModule/controllers/settingsCtrl.js
+++ scripts/modules/appModule/controllers/settingsCtrl.js
@@ -1,10 +1,28 @@
-app.controller('settingsCtrl', ['$scope', '$http', function ($scope, $http) {
+app.controller('settingsCtrl', ['$scope', '$http', 'reportsFactory', function ($scope, $http, reportsFactory) {
+
+    $scope.reports = reportsFactory;
+
+    let successJBox = new jBox('Modal', {
+        id: "jBoxSuccess",
+        width: 400,
+        animation: 'pulse',
+        content: $('#successPopup')
+    });
+
+    let failureJBox = new jBox('Modal', {
+        id: "jBoxFailure",
+        width: 400,
+        animation: 'pulse',
+        content: $('#failurePopup')
+    });
+
 
     $scope.createGroup = function (groupNumber, groupFaculty) {
 
         if (!$scope.groupCreate.$valid)
             return;
 
+        $scope.isLoading = true;
         let req = {
             method: 'POST',
             url: API.urls().groups,
@@ -14,7 +32,19 @@ app.controller('settingsCtrl', ['$scope', '$http', function ($scope, $http) {
             params: { group: {number: groupNumber, faculty: groupFaculty} }
         };
 
-        $http(req).then(function(res) {console.log(res);}, function(res){console.log(res);});
+        $http(req).then(function(res) {
+            // success
+            console.log(res);
+            $scope.isLoading = false;
+            successJBox.open();
+        }, function(res){
+            // failure
+            console.log(res);
+            failureJBox.setContent("<div class='text-center'>" + res.statusText + "</div>");
+
+            $scope.isLoading = false;
+            failureJBox.open();
+        });
 
     };
 
@@ -23,6 +53,7 @@ app.controller('settingsCtrl', ['$scope', '$http', function ($scope, $http) {
         if (!$scope.subjectCreate.$valid)
             return;
 
+        $scope.isLoading = true;
         let req = {
             method: 'POST',
             url: API.urls().subjects,
@@ -32,7 +63,19 @@ app.controller('settingsCtrl', ['$scope', '$http', function ($scope, $http) {
             params: { name: subjectName }
         };
 
-        $http(req).then(function(res) {console.log(res);}, function(res){console.log(res);});
+        $http(req).then(function(res) {
+            // success
+            console.log(res);
+            successJBox.setContent("<div class='text-center'>" + res.statusText + "</div>");
+
+            $scope.isLoading = false;
+            successJBox.open();
+        }, function(res){
+            // failure
+            $scope.isLoading = false;
+            failureJBox.open();
+            console.log(res);
+        });
 
         // let url = 'https://teacher-journal2.herokuapp.com/subjects.json';
         // let req = {subject: {name: subjectName}};
